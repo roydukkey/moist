45a6b3ea8f150715822f54774eb13bbbf79b74b9
roydukkey <trent.dueck@covenanteyes.com>
2022-12-20 15:44:18 -0500

    Create example for vite issue 11012

---
+++ dev/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Text</h1>
+    <ds-text color="blue" weight="bold">Bold Text</ds-text>
+    <ds-text weight="normal">Normal Text</ds-text>
+</template>
+
+<script setup lang="ts" />
---
+++ dev/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+/**
+ * @see {@link https://vitejs.dev/config/}
+ */
+export default defineConfig({
+    plugins: [
+        vue(),
+    ],
+    resolve: {
+        dedupe: [
+            'vue',
+        ],
+    },
+});
